79bf7df5e2bed04739284e9ad6796b6cb1e99608
dredivaris <dredivaris@gmail.com>
2016-03-29 12:46:47 -0700

    add SchemaElement namedtuple, add table creation functionality, update initial test

--- db.py
+++ db.py
@@ -1,11 +1,34 @@
+from collections import namedtuple
+
 import psycopg2
+
+SchemaElement = namedtuple('SchemaElement', 'name datatype')
 
 
 class Database(object):
     conn = psycopg2.connect("dbname='clover' user='dredivaris' host='localhost'")
 
     def __enter__(self):
+        self.conn.autocommit = True
         self.cur = self.conn.cursor()
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.cur.close()
+
+    def fetch_all(self, table_name: str, schema: Schema):
+        self.cur.execute('''SELECT * FROM {table_name};'''.format(table_name=table_name))
+        print(self.cur.fetchall())
+
+    def create_table_if_not_exists(self, table_name: str, schema: list):
+        table_columns = ','.join(['{} {}'.format(item.name, item.datatype) for item in schema])
+        query = '''
+            CREATE TABLE IF NOT EXISTS {table_name}(
+              {table_columns}
+            );'''.format(table_name=table_name, table_columns=table_columns)
+        self.cur.execute(query)
+
+
+
+
+
--- test_system.py
+++ test_system.py
@@ -17,33 +17,41 @@ class TestParser:
 
     def test_parser_basic(self):
         # run command line parser
-        today = date.today().strftime('%Y-%m-%d')
-        datafile = 'data/testformat1_{dt}.txt'.format(dt=today)
-        successful_end = subprocess.call("./file_parser_daemon.py")
-        assert successful_end == 0
+        try:
+            today = date.today().strftime('%Y-%m-%d')
+            specsfile = 'specs/testformat1.csv'
+            datafile = 'data/testformat1_{dt}.txt'.format(dt=today)
+            successful_end = subprocess.call("./file_parser_daemon.py")
+            assert successful_end == 0
 
-        # create spec and data files
-        with open('specs/testformat1.csv', 'w') as f:
-            f.write('test1,width,datatype\nname,10,TEXT\nvalid,1,BOOLEAN\ncount,3,INTEGER')
+            # create spec and data files
+            with open(specsfile, 'w') as f:
+                f.write('test1,width,datatype\nname,10,TEXT\nvalid,1,BOOLEAN\ncount,3,INTEGER')
 
-        # generate date
-        expected_data = [
-            TestComparisonDict({'name': 'Foonyor   ', 'valid': '1', 'count': '  1'}),
-            TestComparisonDict({'name': 'Barzane   ', 'valid': '0', 'count': '-12'}),
-            TestComparisonDict({'name': 'Quuxitude ', 'valid': '1', 'count': '103'}),
-        ]
-        with open(datafile, 'w') as f:
-            for line in expected_data:
-                f.write('{name}{valid}{count}\n'.format(name=line['name'],
-                                                        valid=line['valid'],
-                                                        count=line['count']))
-        sleep(.1)
+            expected_data = [
+                TestComparisonDict({'name': 'Foonyor   ', 'valid': '1', 'count': '  1'}),
+                TestComparisonDict({'name': 'Barzane   ', 'valid': '0', 'count': '-12'}),
+                TestComparisonDict({'name': 'Quuxitude ', 'valid': '1', 'count': '103'}),
+            ]
+            with open(datafile, 'w') as f:
+                for line in expected_data:
+                    f.write('{name}{valid}{count}\n'.format(name=line['name'],
+                                                            valid=line['valid'],
+                                                            count=line['count']))
+            sleep(.1)
 
-        # verify values in db
-        with Database() as db:
-            all_rows = db.fetchall('test1')
-            for row in all_rows:
-                assert row in expected_data
-        # cleanup
-        os.remove('specs/testformat1.csv')
-        os.remove(datafile)
+            # run app
+
+
+            # verify values in db
+            with Database() as db:
+                all_rows = db.fetch_all('test1')
+                for row in all_rows:
+                    assert row in expected_data
+        finally:
+            # cleanup
+            try:
+                os.remove(specsfile)
+                os.remove(datafile)
+            except:
+                pass
