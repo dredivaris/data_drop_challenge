1566830e69865e677d7e3a297b24badcd975c132
dredivaris <dredivaris@gmail.com>
2016-03-29 09:56:47 -0700

    add writing basic spec/data files

--- test_system.py
+++ test_system.py
@@ -7,7 +7,21 @@ class TestParser:
         # run command line parser
         successful_end = subprocess.call("./file_parser_daemon.py")
         assert successful_end == 0
+
         # create spec and data files
+        with open('testformat1.csv', 'w') as f:
+            f.write('test1,width,datatype')
+            f.write('name,10,TEXT')
+            f.write('valid,1,BOOLEAN')
+            f.write('count,3,INTEGER')
+
+        # generate date
+        with open('testformat1_2015-06-28.txt', 'w') as f:
+            f.write('test1,width,datatype')
+            f.write('name,10,TEXT')
+            f.write('valid,1,BOOLEAN')
+            f.write('count,3,INTEGER')
+
         # wait briefly
         # verify values in db
         # cleanup
